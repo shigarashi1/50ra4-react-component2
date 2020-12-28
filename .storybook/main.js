51c16332753abcc42364e926629028038d53a46a
s.igarashi <50ra4.dev.dev@gmail.com>
2020-12-28 20:40:11 +0900

    add: storybook addon

--- .storybook/main.js
+++ .storybook/main.js
@@ -6,6 +6,10 @@ module.exports = {
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
+    "@storybook/addon-actions",
+    "@storybook/addon-a11y",
+    "@storybook/addon-docs",
+    "@storybook/addon-knobs",
     "@storybook/preset-create-react-app"
   ]
 }
