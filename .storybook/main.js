4ccba5eca8b551ab4721afb1537f852bfc1c1c88
sigarashi <48591598+shigarashi1@users.noreply.github.com>
2020-12-28 20:45:28 +0900

    add: storybook setting (#4)
    * apply: sb init
    
    * fix: npm script "build-storybook"
    
    * add: storybook addon

---
+++ .storybook/main.js
@@ -0,0 +1,15 @@
+module.exports = {
+  "stories": [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-actions",
+    "@storybook/addon-a11y",
+    "@storybook/addon-docs",
+    "@storybook/addon-knobs",
+    "@storybook/preset-create-react-app"
+  ]
+}
