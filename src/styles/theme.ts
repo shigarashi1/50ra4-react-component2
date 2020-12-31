87b2c37a06ff428b06039a6822c01343efe288f1
s.igarashi <50ra4.dev.dev@gmail.com>
2020-12-31 17:00:58 +0900

    add: colorPalette & fontColor & buttonColor

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,4 +1,7 @@
 const colorPalette = {
+  default: {
+    main: '#708090',
+  },
   // https://material.io/resources/color/#!/?view.left=0&view.right=0&primary.color=263238&secondary.color=2E7D32
   primary: {
     main: '#263228',
@@ -10,18 +13,49 @@ const colorPalette = {
     light: '#60ac5d',
     dark: '#004f04',
   },
+  positive: {
+    main: '#3cb371',
+  },
+  negative: {
+    main: '#708090',
+  },
+  success: {
+    main: '#3cb371',
+  },
+  error: {
+    main: '#dc143c',
+  },
+  warning: {
+    main: '#ffa500',
+  },
+  info: {
+    main: '#1e90ff',
+  },
 };
 
 const fontColor = {
+  default: '#ffffff',
   primary: '#ffffff',
   secondary: '#ffffff',
+  white: '#ffffff',
+  black: '#000000',
+  gray: '#808080',
+  lightGray: '#d3d3d3',
 };
 export type FontColor = keyof typeof fontColor;
 
 const buttonColor = {
+  default: colorPalette.default.main,
   primary: colorPalette.primary.main,
   secondary: colorPalette.secondary.main,
+  positive: colorPalette.positive.main,
+  negative: colorPalette.negative.main,
+  success: colorPalette.success.main,
+  error: colorPalette.error.main,
+  warning: colorPalette.warning.main,
+  info: colorPalette.info.main,
 };
+export type ButtonColor = keyof typeof buttonColor;
 
 export const lightTheme = {
   color: {
