7df5d35002ed2a20f0016c83f543320a05ffe951
s.igarashi <50ra4.dev.dev@gmail.com>
2021-01-01 18:33:52 +0900

    add: MenuIcon

---
+++ src/components/display/Icons/MenuIcon.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { SvgIcon, IconProps } from './SvgIcon';
+
+export const MenuIcon: React.FC<IconProps> = ({ className, titleAccess, color, size }) => {
+  return (
+    <SvgIcon className={className} titleAccess={titleAccess} color={color} size={size}>
+      <path d="M0 0h24v24H0z" fill="none" />
+      <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z" />
+    </SvgIcon>
+  );
+};
