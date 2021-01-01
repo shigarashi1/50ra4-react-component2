60dc5b607f5ae3b250ec1b221f174969357d56fc
s.igarashi <50ra4.dev.dev@gmail.com>
2021-01-01 18:09:07 +0900

    add: ArrowLeftIcon

---
+++ src/components/display/Icons/ArrowLeftIcon.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { SvgIcon, IconProps } from './SvgIcon';
+
+export const ArrowLeftIcon: React.FC<IconProps> = ({ className, titleAccess, color, size }) => {
+  return (
+    <SvgIcon className={className} titleAccess={titleAccess} color={color} size={size}>
+      <path d="M0 0h24v24H0V0z" fill="none" />
+      <path d="M15.41 16.59L10.83 12l4.58-4.59L14 6l-6 6 6 6 1.41-1.41z" />
+    </SvgIcon>
+  );
+};
