b9484b71f940128f13f5fd21062661f10a6dfd5f
s.igarashi <50ra4.dev.dev@gmail.com>
2021-01-02 14:32:33 +0900

    fix: AddIcon

--- src/components/display/Icons/AddIcon.tsx
+++ src/components/display/Icons/AddIcon.tsx
@@ -6,7 +6,7 @@ export const AddIcon: React.FC<IconProps> = ({ className, titleAccess, color, si
   return (
     <SvgIcon className={className} titleAccess={titleAccess} color={color} size={size}>
       <path d="M0 0h24v24H0z" fill="none" />
-      <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-6h2v6zm0-8h-2V7h2v2z" />
+      <path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z" />
     </SvgIcon>
   );
 };
