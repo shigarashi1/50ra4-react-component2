475cc28c6d118a3a083fee30915b1289dca98ff6
sigarashi <48591598+shigarashi1@users.noreply.github.com>
2021-01-06 23:08:33 +0900

    Revert "fix: ClearIconButton (#52)"
    This reverts commit 1fcf2d5a1e5cb3ca2f40ac85a4b3c7f74551aa6c.

--- src/components/inputs/ClearIconButton/ClearIconButton.tsx
+++ src/components/inputs/ClearIconButton/ClearIconButton.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
+import styled from 'styled-components';
 
 import { HighLightOffIcon } from '../../display';
 import { IconButtonProps, IconButton } from '../IconButton/IconButton';
 
 type Props = IconButtonProps;
 
-export const ClearIconButton: React.FC<Props> = (props) => {
+const ClearIconButtonBase: React.FC<Props> = (props) => {
   return (
     <IconButton {...props}>
       <HighLightOffIcon color="default" size="small" />
     </IconButton>
   );
 };
+
+export const ClearIconButton = styled(ClearIconButtonBase)`
+  border: none;
+  outline: none;
+`;
