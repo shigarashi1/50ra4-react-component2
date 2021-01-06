1fcf2d5a1e5cb3ca2f40ac85a4b3c7f74551aa6c
sigarashi <48591598+shigarashi1@users.noreply.github.com>
2021-01-06 22:17:52 +0900

    fix: ClearIconButton (#52)
    * fix: ClearIconButton
    
    * update: storybook snapshot file

--- src/components/inputs/ClearIconButton/ClearIconButton.tsx
+++ src/components/inputs/ClearIconButton/ClearIconButton.tsx
@@ -1,20 +1,14 @@
 import React from 'react';
-import styled from 'styled-components';
 
 import { HighLightOffIcon } from '../../display';
 import { IconButtonProps, IconButton } from '../IconButton/IconButton';
 
 type Props = IconButtonProps;
 
-const ClearIconButtonBase: React.FC<Props> = (props) => {
+export const ClearIconButton: React.FC<Props> = (props) => {
   return (
     <IconButton {...props}>
       <HighLightOffIcon color="default" size="small" />
     </IconButton>
   );
 };
-
-export const ClearIconButton = styled(ClearIconButtonBase)`
-  border: none;
-  outline: none;
-`;
